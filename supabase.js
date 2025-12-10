e7e0c8a05bfe98a06bba6153c2be84f7cb311415
dkogan-coder <dkogan@klretailsolutions.com>
2025-12-10 15:33:42 -0600

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://qmvtklemligxkicygfeq.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFtdnRrbGVtbGlneGtpY3lnZmVxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUzMjc1MjMsImV4cCI6MjA4MDkwMzUyM30.2_tQmEv4jPDhW_7VEASkcsMKwYG7RDSDR1m73CP1OVU'
+const supabaseUrl = 'https://kcmepfywnhozfilpdrfn.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtjbWVwZnl3bmhvemZpbHBkcmZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU0MDEwMzksImV4cCI6MjA4MDk3NzAzOX0.5CCUg17D1e6Q0uXfTpiZjWBy3AolgdOI6cctkibg4rE';
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
